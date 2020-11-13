035b60ae289a90d3dd552766a8f1b75f6ce55f9e
bojegowda671@gmail.com <bojegowda671@gmail.com>
2020-11-13 16:16:48 +0530

    added kotlin code

--- app/src/main/java/com/java2blog/webviewexampleapp/MainActivity.java
+++ app/src/main/java/com/java2blog/webviewexampleapp/MainActivity.java
@@ -1,50 +1,24 @@
-package com.java2blog.webviewexampleapp;
-
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-public class MainActivity extends AppCompatActivity {
-
-    private WebView webView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        webView = (WebView) findViewById(R.id.webView);
-        webView.setWebViewClient(new myWebClient());
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://spoorthiadsbucket.s3-website.ap-south-1.amazonaws.com/#/");
-
-    }
-
-    public class myWebClient extends WebViewClient
-    {
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
-        }
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-
-            view.loadUrl(url);
-            return true;
-
-        }
-    }
-
-    @Override
-    // This method is used to detect back button
-    public void onBackPressed() {
-        if(webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            // Let the system handle the back button
-            super.onBackPressed();
-        }
-    }
+import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
+   private val webView: WebView? = null
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+      title = "KotlinApp"
+      val webView = findViewById<WebView>(R.id.webView)
+      webView.webViewClient = WebViewClient()
+      webView.loadUrl("http://spoorthiadsbucket.s3-website.ap-south-1.amazonaws.com/#/")
+      val webSettings = webView.settings
+      webSettings.javaScriptEnabled = true
+   }
+   override fun onBackPressed() {
+      if (webView!!.canGoBack()) {
+         webView.goBack()
+      } else {
+         super.onBackPressed()
+      }
+   }
 }
